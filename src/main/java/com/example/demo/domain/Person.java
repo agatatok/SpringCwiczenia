189a6df6a311371b1466552c6a5cdff7d41bc4ca
agatatok <agattok260@student.polsl.pl>
2020-04-01 21:18:13 +0200

    Init + basic classes

---
+++ src/main/java/com/example/demo/domain/Person.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    private Long id;
+
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @NonNull
+    private int wiek;
+}
