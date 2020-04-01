fd47a917413cbbabd7edbae6b3de2e42d8f5f076
agatatok <agattok260@student.polsl.pl>
2020-04-01 21:29:54 +0200

    PersonRepository interface

--- src/main/java/com/example/demo/repository/PersonRepository.java
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface PersonRepository {
+import com.example.demo.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
