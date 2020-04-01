47f7dedcf256e0d8f6e5ec621895a2d91a0aa611
agatatok <agattok260@student.polsl.pl>
2020-04-01 21:32:56 +0200

    Person Controller

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Person;
+import com.example.demo.repository.PersonRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonController {
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping("/person/{id}")
+    public Person getPerson(Long id){
+        return personRepository.findById(id).get();
+    }
+
+
 }
